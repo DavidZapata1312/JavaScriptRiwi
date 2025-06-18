1f5fb1e225b131730559b67244bd25db3e41777b
David Zapata <davidalejandrozapata2@gmail.com>
2025-06-18 08:13:05 -0500

    Actualizar logica.js

--- semana1/logica.js
+++ semana1/logica.js
@@ -1,21 +1,23 @@
-let lado1 =parseInt(prompt("Ingresa el valor del primer lado")) ;
+let lado1 = parseInt(prompt("Ingresa el valor del primer lado"));
 let lado2 = parseInt(prompt("Ingresa el valor del segundo lado"));
-let lado3 = parseInt(prompt("Ingresa el valor del ultimo lado"));
-let h = (lado1 + lado2);
-console.log(h)
+let lado3 = parseInt(prompt("Ingresa el valor del último lado"));
 
-if (lado3 <= h){
-    if ((lado1===lado2) && (lado1===lado3)) {
-        console.log("Triangulo equilatero");
-        
-    } else if((lado1===lado2) && (lado1===lado3)&&(lado2===lado3)){
-        console.log("Triangulo Isoceles")
-    } else{
-        console.log("Triangulo escaleno")
+console.log(`Lados ingresados: ${lado1}, ${lado2}, ${lado3}`);
+
+// Validar si los lados forman un triángulo real
+if (
+    lado1 + lado2 > lado3 &&
+    lado1 + lado3 > lado2 &&
+    lado2 + lado3 > lado1
+) {
+    // Clasificar el triángulo
+    if (lado1 === lado2 && lado2 === lado3) {
+        console.log("Triángulo equilátero");
+    } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+        console.log("Triángulo isósceles");
+    } else {
+        console.log("Triángulo escaleno");
     }
-} else{
-    console.error("¡Ups! No es un triangulo");
+} else {
+    console.error("¡Ups! Los valores no forman un triángulo válido");
 }
-
-
-
