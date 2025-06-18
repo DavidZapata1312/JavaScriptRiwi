80dfa63bd73939a3c65c7d2f43eaaec0554a83f4
David Zapata <davidalejandrozapata2@gmail.com>
2025-06-18 08:14:09 -0500

    change logic for better validation in triangle differentiation

--- semana1/logica.js
+++ semana1/logica.js
@@ -1,21 +1,19 @@
-let lado1 =parseInt(prompt("Ingresa el valor del primer lado")) ;
+let lado1 = parseInt(prompt("Ingresa el valor del primer lado"));
 let lado2 = parseInt(prompt("Ingresa el valor del segundo lado"));
 let lado3 = parseInt(prompt("Ingresa el valor del ultimo lado"));
 let h = (lado1 + lado2);
 console.log(h)
-
-if (lado3 <= h){
-    if ((lado1===lado2) && (lado1===lado3)) {
+if (lado3 <= h) {
+    if ((lado1 === lado2) && (lado1 === lado3)) {
         console.log("Triangulo equilatero");
-        
-    } else if((lado1===lado2) && (lado1===lado3)&&(lado2===lado3)){
+
+    } else if ((lado1 === lado2) || (lado1 === lado3) || (lado2 === lado3)) {
         console.log("Triangulo Isoceles")
-    } else{
+    } else {
         console.log("Triangulo escaleno")
     }
-} else{
+} else {
     console.error("¡Ups! No es un triangulo");
 }
-
 
 
